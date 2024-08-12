1e60666f2f1f97bc409c192113f9d20c50abd9ef
SayyantR <sayyant@semgrep.com>
2024-08-12 14:19:25 -0700

    remade repo

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-from fake_useragent import settings
-
-logging.basicConfig(level=logging.DEBUG)
-
-settings.HTTP_DELAY = 0
--- tests/test_errors.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from fake_useragent import errors
-
-
-class TestErrors(unittest.TestCase):
-    def test_error_aliases(self):
-        assert errors.FakeUserAgentError is errors.UserAgentError
